8ca3f46dcda52423a0247a2089a665f5d013ab4f
jackytianhappy <1142899950@qq.com>
2016-09-20 00:36:30 +0800

    调整顶部的文字

--- NewsApp/NewsApp/src/tabbarVC/homeNavi.js
+++ NewsApp/NewsApp/src/tabbarVC/homeNavi.js
@@ -1,13 +1,32 @@
 import React ,{Component} from 'react';
 import {
-  Navigator
+  Navigator,
+  Text,
+  StyleSheet
 } from 'react-native';
 
 import Home from '../home/home.js'
 
+
+var NavigationBarRouteMapper = {
+LeftButton: function (route, navigator, index, navState) {
+    return null;
+},
+RightButton: function (route, navigator, index, navState) {
+    return null;
+},
+Title: function (route, navigator, index, navState) {
+    return (
+        <Text style = {styles.naviTitleText}>首页</Text>
+    );
+},
+};
+
+
 export default class HomeNavi extends Component {
   constructor(props) {
     super(props);
+
   }
 
   render(){
@@ -20,12 +39,31 @@ export default class HomeNavi extends Component {
                    let Com = route.componet;
                    return <Com {...route.params} navigator = {navigator}></Com>
                  }}
-
-                 
+                navigationBar={
+                              <Navigator.NavigationBar
+                              title = '首页'
+                              routeMapper={NavigationBarRouteMapper}
+                              style={{backgroundColor:'darkslateblue',flex:1}}
+                              {...this.props}/>
+                              }
                  >
 
       </Navigator>
     );
   }
-
 }
+
+const styles = StyleSheet.create({
+  navigationBar : {
+    backgroundColor : 'darkslateblue',
+    flex : 1
+  },
+  naviTextView :{
+
+  },
+  naviTitleText : {
+    color : 'white',
+    fontSize : 16,
+    marginVertical : 10
+  }
+});
